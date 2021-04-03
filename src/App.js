ec20763d2744369064657aec0b11f1b0524a2f6b
Rafat <rafatiqbalsiddiqui@gmail.com>
2021-04-03 03:27:59 -0600

    [rares-clothing]--added style for homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from './HomePage.Component'
+import HomePage from './homepage.component'
 
 function App() {
   return (
--- src/HomePage.Component.jsx
+++ src/HomePage.Component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './homepage.styles.scss'
 
 const HomePage=()=>(
   <div className="homepage">
@@ -8,7 +9,31 @@ const HomePage=()=>(
           <div className="content">
             <div className="title">HATS</div>   
             <span className="subtitle">SHOP NOW</span>
-          </div>
+          </div>          
+      </div>
+      <div className="menu-item">
+          <div className="content">
+            <div className="title">JACKETS</div>   
+            <span className="subtitle">SHOP NOW</span>
+          </div>          
+      </div>
+      <div className="menu-item">
+          <div className="content">
+            <div className="title">SHOES</div>   
+            <span className="subtitle">SHOP NOW</span>
+          </div>          
+      </div>
+      <div className="menu-item">
+          <div className="content">
+            <div className="title">WOMEN</div>   
+            <span className="subtitle">SHOP NOW</span>
+          </div>          
+      </div>
+      <div className="menu-item">
+          <div className="content">
+            <div className="title">MEN</div>   
+            <span className="subtitle">SHOP NOW</span>
+          </div>          
       </div>
     </div>
   </div>
