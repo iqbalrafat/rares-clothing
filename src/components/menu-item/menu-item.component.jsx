70f0f58b4903f673b59e767c19f077eb7bc07be0
Rafat <iqbalrafat@yahoo.com>
2021-04-04 00:00:35 -0600

    [rares-clothing]--added Images

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 //we can use prop to replace hard coaded title etc or we can refactor just the title etc that we need.
-const MenuItem=({title})=>(
-  <div className="menu-item">
+const MenuItem=({title, imageUrl,size})=>(
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
     <div className="content">
       <h1 className="title">{title}</h1>   
       <span className="subtitle">SHOP NOW</span>
