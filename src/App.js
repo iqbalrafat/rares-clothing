194e91d1467d23700e60ed3b1f2a62dc4e794a9e
iqbalrafat <iqbalrafat@yahoo.com>
2021-04-07 11:37:02 -0600

    [rares_clothing]-- added shop component and route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-       <HomePage />  
+    <Switch>
+       <Route exact path="/" component={HomePage} />
+       <Route exact path="/shop" component={ShopPage} />    
+    </Switch>       
   );
 }
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -38,14 +38,14 @@ class Directory extends Component {
         id: 5,
         linkUrl: 'shop/mens'
       }
-    ]
+     ]
     };
   }
   render() {
     return(
     <div className="directory-menu">
-      {this.state.sections.map(({ title, imageUrl, id, size }) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      {this.state.sections.map(({ title, imageUrl, id, size}) => (
+        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
       ))}  
     </div> 
     );
