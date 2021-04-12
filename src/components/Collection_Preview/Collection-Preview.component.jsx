0afbded5ef3a8c9bb1b40640e6fb954cf90019f5
iqbalrafat <iqbalrafat@yahoo.com>
2021-04-12 14:07:07 -0600

    [rares-clothing]-- added Collection_Item Component

--- src/components/Collection_Preview/Collection-Preview.component.jsx
+++ src/components/Collection_Preview/Collection-Preview.component.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import './Collection-Preview.styles.scss'
+import CollectionItem from '../Collection-Item/Collection-Item.component'
 
-const CollectionPreview=({title,items})=>(
+const CollectionPreview=({title,items})=>
+(
   <div className="Collection-Preview">
     <h1 className="title">{title.toUpperCase()}</h1>
       <div className="Preview"> 
          {
            items
            .filter((item,idx)=>idx< 4)
-           .map(item =>(
-           <div key={item.id}>{item.name}</div>
-           ))}
+           .map(item =>
+            <CollectionItem
+            key  = {item.id}
+            name = {item.name}
+            imageUrl={item.imageUrl}
+            price = {item.price} />
+           )
+         }           
       </div>
   </div>
 );
-
 export default CollectionPreview;
