489ae9a77e29d6210e2c2195171f6dfdce489a52
iqbalrafat <iqbalrafat@yahoo.com>
2021-04-19 23:46:18 -0600

    [rares-clothing]--updated Header component

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from '../../firebase/firebase.utils.js'
 import "./Header.styles.scss";
 function Header(props) {
   return (
@@ -15,11 +16,20 @@ function Header(props) {
         <Link to="/" className="option">
           CONTACT
         </Link>
-        <Link to="/SignIn" className="option">
-          SIGN-IN
-        </Link>
+        {
+          props.currentUser ? 
+          (<div className="option" onClick= { () => auth.signOut()}>
+            SIGN OUT
+          </div>) : 
+          (
+              <Link className="option" to="/SignIn">
+                  SIGN IN
+              </Link>
+          )
+      }
       </div>
-    </div>
+
+  </div>
   );
 }
 export default Header;
